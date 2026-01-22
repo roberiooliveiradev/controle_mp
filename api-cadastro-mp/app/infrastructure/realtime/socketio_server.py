71b3ee3f80b2a7da15b4df6a41b7aa593c5b53be
roberiooliveiradev <roberio.oliveira.trabalho@gmail.com>
2026-01-22 18:12:58 -0300

    Ajuste do socketio io

--- api-cadastro-mp/app/infrastructure/realtime/socketio_server.py
+++ api-cadastro-mp/app/infrastructure/realtime/socketio_server.py
@@ -3,13 +3,18 @@ from __future__ import annotations
 
 from flask_socketio import SocketIO
 
-# socketio = SocketIO(
-#     cors_allowed_origins="*",
-#     async_mode="threading",
-#     logger=True,
-#     engineio_logger=True,
-# )
 socketio = SocketIO(
-    cors_allowed_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    cors_allowed_origins=[
+        # DEV local
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+
+        # LAN (produção interna)
+        "http://192.168.1.237",
+
+        # opcional: algumas máquinas acessam por hostname local
+        "http://srv-api",
+        "http://srv-api.local",
+    ],
     async_mode="eventlet",
 )
